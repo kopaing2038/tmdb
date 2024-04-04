07bc86a5cdfe99ca47902e78a446fbcb739ad3f1
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-04-04 09:48:43 +0630

    Update client.py

--- client.py
+++ client.py
@@ -2,10 +2,8 @@ from swibots import Client
 import config
 
 app = Client(
-     name=config.SESSION,
-     api_id=config.API_ID,
-     api_hash=config.API_HASH,
-     token=config.BOT_TOKEN,  # Make sure to include the token argument
-     workers=50,
-     plugins={"root": "plugins"},
+    config.BOT_TOKEN,
+    plugins=dict(
+        root="plugins"
+    )
 )
